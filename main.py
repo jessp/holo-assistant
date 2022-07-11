a4ef7e36acbf0cc51454eb80bcd93699de35b8c9
Jess <jessicapeter92@gmail.com>
2022-07-11 12:44:56 +0100

    working on deepspeech recognizer, subbing out pyaudio for sounddevice

--- main.py
+++ main.py
@@ -1,100 +1,172 @@
-# # https://medium.com/slanglabs/how-to-build-python-transcriber-using-mozilla-deepspeech-5485b8d234cf
-# # https://medium.com/slanglabs/automatic-speech-recognition-in-python-programs-a64851ad29b3
-
-# import deepspeech
-# import numpy as np
-# import os
-# import sounddevice as sd
-# import time
-# import sys
-# import queue
-
-# blocksize = 1024
-
-# # DeepSpeech parameters
-# model = deepspeech.Model('deepspeech-0.9.3-models.pbmm')
-# model.enableExternalScorer('deepspeech-0.9.3-models.scorer')
-
-# # Create a Streaming session
-# context = model.createStream()
-
-# device_info = sd.query_devices(0, 'input')
-# # soundfile expects an int, sounddevice provides a float:
-# samplerate = int(device_info['default_samplerate'])
-# # Encapsulate DeepSpeech audio feeding into a callback for PyAudio
-# text_so_far = ''
-
-# def process_audio(in_data, frames, time, status):
-#     try:
-#         assert frames == blocksize
-#         if status.input_underflow:
-#             print('Input underflow: increase blocksize?', file=sys.stderr)
-#             raise sd.CallbackAbort
-#         global text_so_far
-#         data16 = np.frombuffer(in_data, dtype=np.int16)
-#         context.feedAudioContent(data16)
-#         text = context.intermediateDecode()
-#         if text != text_so_far:
-#             print('Interim text = {}'.format(text))
-#             text_so_far = text
-#     except:
-#         print("Ended callback")
-
-
-# stream = sd.InputStream(samplerate=samplerate, device=0, channels=1, blocksize=blocksize, callback=process_audio)
-
-# print('Please start speaking, when done press Ctrl-C ...')
-# stream.start()
-
-# try:
-#     while (stream.active):
-#         time.sleep(0.1)
-# except KeyboardInterrupt:
-#     print('Finished recording.')
-#     # DeepSpeech
-#     stream.stop()
-#     text = context.finishStream()
-#     print('Final text = {}'.format(text))
-
-
-import deepspeech as ds
+import time, logging
+from datetime import datetime
+import threading, collections, queue, os, os.path
+import deepspeech
+import numpy as np
+# import pyaudio
 import sounddevice as sd
-import os
+import wave
+import webrtcvad
+from halo import Halo
+from scipy import signal
 
-# https://deepspeech.readthedocs.io/en/v0.9.3/Python-Examples.html
-#print("Loading model...")
-model = ds.Model('deepspeech-0.9.3-models.pbmm')
-#print("Loading scorer...")
-model.enableExternalScorer('deepspeech-0.9.3-models.scorer')
+logging.basicConfig(level=20)
 
-def stt(audio = None, temp = None):
-    try:
-        fs = 16000
-        # https://python-sounddevice.readthedocs.io/en/latest/usage.html#recording
+class Audio(object):
+    """Streams raw audio from microphone. Data is received in a separate thread, and stored in a buffer, to be read from."""
 
-        duration = 10 # seconds
-        #print("Recording now...")
-        audioData = sd.rec(int(duration * fs), samplerate=fs, channels=1, dtype="int16")
-        #audioData = sd.rec(samplerate=fs, channels=1, dtype="int16")
-        sd.wait()
-        #print("Done recording!")
+    DTYPE = "int16"
+    # Network/VAD rate-space
+    RATE_PROCESS = 16000
+    CHANNELS = 1
+    BLOCKS_PER_SECOND = 50
 
-        audioData = list(map(lambda channels : channels[0], audioData))
+    def __init__(self, callback=None, input_rate=RATE_PROCESS):
+        def proxy_callback(in_data, frame_count, time_info, status):
+            #pylint: disable=unused-argument
+            if self.chunk is not None:
+                in_data = self.wf.readframes(self.chunk)
+            callback(in_data)
+            # return (None)
+        if callback is None: 
+            callback = lambda in_data: self.buffer_queue.put(in_data)
+        self.buffer_queue = queue.Queue()
+        self.input_rate = input_rate
+        self.sample_rate = self.RATE_PROCESS
+        self.block_size = int(self.RATE_PROCESS / float(self.BLOCKS_PER_SECOND))
+        self.block_size_input = int(self.input_rate / float(self.BLOCKS_PER_SECOND))
+        self.pa = sd
 
-        #print("Processing...")
-        text = model.stt(audioData)
-        print(text)
-        return text
-    except KeyboardInterrupt:
-        print("\nDetermining SpeechToText engine...")
-    except Exception as e:
-        print(e)
+        kwargs = {
+            'dtype': self.DTYPE,
+            'channels': self.CHANNELS,
+            'samplerate': self.sample_rate,
+            'callback': proxy_callback,
+            'blocksize': self.block_size_input
+        }
+
+
+
+        self.chunk = None
+        # if not default device
+
+        # kwargs['input_device_index'] = self.device
+
+        # self.stream = self.pa.open(**kwargs)
+        self.stream = self.pa.InputStream(**kwargs)
+        self.stream.start()
+
+    def resample(self, data, input_rate):
+        """
+        Microphone may not support our native processing sampling rate, so
+        resample from input_rate to RATE_PROCESS here for webrtcvad and
+        deepspeech
+
+        Args:
+            data (binary): Input audio stream
+            input_rate (int): Input audio rate to resample from
+        """
+        data16 = np.fromstring(string=data, dtype=np.int16)
+        resample_size = int(len(data16) / self.input_rate * self.RATE_PROCESS)
+        resample = signal.resample(data16, resample_size)
+        resample16 = np.array(resample, dtype=np.int16)
+        return resample16.tostring()
+
+    def read_resampled(self):
+        """Return a block of audio data resampled to 16000hz, blocking if necessary."""
+        return self.resample(data=self.buffer_queue.get(),
+                             input_rate=self.input_rate)
+
+    def read(self):
+        """Return a block of audio data, blocking if necessary."""
+        return self.buffer_queue.get()
+
+    def destroy(self):
+        self.stream.stop_stream()
+        self.stream.close()
+        self.pa.terminate()
+
+    frame_duration_ms = property(lambda self: 1000 * self.block_size // self.sample_rate)
+
+
+class VADAudio(Audio):
+    """Filter & segment audio with voice activity detection."""
+
+    def __init__(self, aggressiveness=3, input_rate=None):
+        super().__init__(input_rate=input_rate)
+        self.vad = webrtcvad.Vad(aggressiveness)
+
+    def frame_generator(self):
+        """Generator that yields all audio frames from microphone."""
+        if self.input_rate == self.RATE_PROCESS:
+            while True:
+                # yield self.read()
+                yield self.read_resampled()
+        else:
+            while True:
+                yield self.read_resampled()
+
+    def vad_collector(self, padding_ms=300, ratio=0.75, frames=None):
+        """Generator that yields series of consecutive audio frames comprising each utterence, separated by yielding a single None.
+            Determines voice activity by ratio of frames in padding_ms. Uses a buffer to include padding_ms prior to being triggered.
+            Example: (frame, ..., frame, None, frame, ..., frame, None, ...)
+                      |---utterence---|        |---utterence---|
+        """
+        if frames is None: frames = self.frame_generator()
+        num_padding_frames = padding_ms // self.frame_duration_ms
+        ring_buffer = collections.deque(maxlen=num_padding_frames)
+        triggered = False
+
+        for frame in frames:
+            if len(frame) < 640:
+                return
+            is_speech = self.vad.is_speech(frame, self.sample_rate)
+
+            if not triggered:
+                ring_buffer.append((frame, is_speech))
+                num_voiced = len([f for f, speech in ring_buffer if speech])
+                if num_voiced > ratio * ring_buffer.maxlen:
+                    triggered = True
+                    for f, s in ring_buffer:
+                        yield f
+                    ring_buffer.clear()
+
+            else:
+                yield frame
+                ring_buffer.append((frame, is_speech))
+                num_unvoiced = len([f for f, speech in ring_buffer if not speech])
+                if num_unvoiced > ratio * ring_buffer.maxlen:
+                    triggered = False
+                    yield None
+                    ring_buffer.clear()
+
+def main():
+    DEFAULT_SAMPLE_RATE = 16000
+    # Load DeepSpeech model
+    print('Initializing model...')
+    model = deepspeech.Model('deepspeech-0.9.3-models.pbmm')
+    model.enableExternalScorer('deepspeech-0.9.3-models.scorer')
+
+
+    # Start audio with VAD
+    vad_audio = VADAudio(aggressiveness=3,
+                         input_rate=DEFAULT_SAMPLE_RATE)
+    print("Listening (ctrl-C to exit)...")
+    frames = vad_audio.vad_collector()
+    # Stream from microphone to DeepSpeech using VAD
+    spinner = Halo(spinner='line')
+    stream_context = model.createStream()
+    for frame in frames:
+        if frame is not None:
+            if spinner: spinner.start()
+            logging.debug("streaming frame")
+            stream_context.feedAudioContent(np.frombuffer(frame, np.int16))
+        else:
+            if spinner: spinner.stop()
+            logging.debug("end utterence")
+            text = stream_context.finishStream()
+            print("Recognized: %s" % text)
+            stream_context = model.createStream()
 
 if __name__ == '__main__':
-    wakeword = "test"
-    text = str(stt())
-    print(text)
-    print("Wakeword: " + wakeword)
-    if text.strip().lower().find(wakeword.replace("\n", "").strip().lower()) != -1:
-        print("Wake-word was found in spoken text")
-    print("Determining audio listener...")
+    main()
