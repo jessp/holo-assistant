6bced54ec05222f1cc023193255adffbfb03641e
jessp <jessicapeter92@gmail.com>
2022-09-25 19:58:19 +0100

    Updates

---
+++ precache-manifest.d2019ca10283e95b3b44fbff1c168f34.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc4e80a412e8975bc352",
+    "url": "/holo-assistant/static/css/main.c9fc1a51.chunk.css"
+  },
+  {
+    "revision": "fc4e80a412e8975bc352",
+    "url": "/holo-assistant/static/js/main.fc4e80a4.chunk.js"
+  },
+  {
+    "revision": "41e4ec44fab7740fb5a5",
+    "url": "/holo-assistant/static/js/1.41e4ec44.chunk.js"
+  },
+  {
+    "revision": "c6b59ac924bbcfa55bd6",
+    "url": "/holo-assistant/static/js/runtime~main.c6b59ac9.js"
+  },
+  {
+    "revision": "fc18eb3c29c6b914d9bfb2b0e584210c",
+    "url": "/holo-assistant/static/media/maria.fc18eb3c.svg"
+  },
+  {
+    "revision": "5a595349e88ce28e862843b56e67de73",
+    "url": "/holo-assistant/static/media/instructions.5a595349.svg"
+  },
+  {
+    "revision": "617af5cf6c7a56ba558c16b1c56e5c5b",
+    "url": "/holo-assistant/static/media/bottom-ornamental-frame.617af5cf.svg"
+  },
+  {
+    "revision": "333e696532b91ccd3d58fbaad874e04d",
+    "url": "/holo-assistant/static/media/top-ornamental-frame.333e6965.svg"
+  },
+  {
+    "revision": "0062a8b57fcf2409ba5a4b7fcf03c519",
+    "url": "/holo-assistant/static/media/drawing.0062a8b5.png"
+  },
+  {
+    "revision": "940d9d48d3b279d878a6740fb5632bc8",
+    "url": "/holo-assistant/static/media/dark-heart.940d9d48.svg"
+  },
+  {
+    "revision": "16a2ae42797f85894ef42447afabf2c2",
+    "url": "/holo-assistant/static/media/mid-heart.16a2ae42.svg"
+  },
+  {
+    "revision": "59d0ca6c12d78d09a96249f65f455b5d",
+    "url": "/holo-assistant/index.html"
+  }
+];
