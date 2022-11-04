718c5ec1a5eb0e6c23b865da0ed1835e7dba6b2c
jessp <jessicapeter92@gmail.com>
2022-11-04 20:27:04 +0000

    added basic analytics without cookies

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import PiwikPro from '@piwikpro/react-piwik-pro';
+
+PiwikPro.initialize('34c68ac1-60e3-479c-99fb-45fb49a2e649', 'https://awardwinninghuman.containers.piwik.pro');
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
