cb607168ea8a1c43f28dbba8e4a92cec6c4b5312
jessp <jessicapeter92@gmail.com>
2023-03-19 19:13:51 +0000

    switched to a more human-sounding api

--- serverAudio/dictionaryskill.py
+++ serverAudio/dictionaryskill.py
@@ -20,12 +20,15 @@ class DictionarySkill(CharacterController):
 		elif dictionary_resp[1] == 0:
 			self.talk("not_a_word.wav")
 		else:
-			self.talk("found_definition.wav")
+			if (dictionary_resp[1] == 2):
+				self.talk("found_definition.wav")
+			else:
+				self.talk("found_definitions.wav")
 			self.synthesize_text(dictionary_resp[1])
 			self.talk("latest_output.wav")
 
 	def get_definition(self, key, text, terms):
-		definition_base_url = "https://api.api-ninjas.com/v1/dictionary?word="
+		definition_base_url = "https://wordsapiv1.p.rapidapi.com/words/%s/definitions"
 		found_word = ""
 		for term in terms:
 			attempt = text.partition(term)[2]
@@ -35,13 +38,30 @@ class DictionarySkill(CharacterController):
 		if found_word == "":
 			return (-1)
 		else:
-			definition_response = requests.get((definition_base_url + found_word), headers={'X-Api-Key': self.dictionary_key})
+			full_url = definition_base_url%(found_word)
+			print(full_url)
+			definition_response = requests.get(full_url, \
+				headers={"X-RapidAPI-Key": self.dictionary_key, "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com"}, \
+				verify=False)
+			print(definition_response)
 			if definition_response.status_code == requests.codes.ok:
 				json_resp = definition_response.json()
-				if json_resp["valid"] == True:
-					return (1, json_resp["definition"])
+				definitions = [(x["definition"]) for x in json_resp["definitions"]]
+				if len(definitions) == 1:
+					#only return the first few definitions
+					return (2, definitions[:5])
 				else:
-					return (0)
+					return (1, (". ").join(definitions))
 			else:
+				json_resp = definition_response.json()
+				try:
+					#special error message for non-word, but otherwise send something generic
+					json_resp["message"]
+					if json_resp["message"] == "word not found":
+						return (0)
+					else:
+						return (-1)
+				except:
+					return (-1)
 				return (-1)
 		
